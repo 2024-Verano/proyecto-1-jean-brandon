db3de56829a97d2844c4dbe24b380909b871e1ed
BrandonVB09 <163044581+BrandonVB09@users.noreply.github.com>
2024-12-18 16:03:55 -0600

    Prueba

---
+++ Project_1/src/project_1/Customers.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project_1;
+
+/**
+ *
+ * @author brand
+ */
+public class Customers {
+    protected Integer costumer_code;
+    String costumer_name;
+    String last_namec;
+    Integer phone_number; 
+    String costumer_email;
+    String costumer_province;
+    String costumer_canton;
+    String costumer_distict;
+    Integer birth_date;
+
+    public Customers(Integer costumer_code, String costumer_name, String last_namec, Integer phone_number, String costumer_email, String costumer_province, String costumer_canton, String costumer_distict, Integer birth_date) {
+        this.costumer_code = costumer_code;
+        this.costumer_name = costumer_name;
+        this.last_namec = last_namec;
+        this.phone_number = phone_number;
+        this.costumer_email = costumer_email;
+        this.costumer_province = costumer_province;
+        this.costumer_canton = costumer_canton;
+        this.costumer_distict = costumer_distict;
+        this.birth_date = birth_date;
+    }
+    public void serch(){
+    
+    }
+    public void add(){
+        
+    }
+    public void alter(){
+        
+    }
+    public void remove(){
+        
+    }
+    
+}
