cbfdd972f0a807f1012ae527c5c27a9797597f87
brand <brand@Compu_Bran>
2024-12-23 17:10:15 -0600

    prueba clientes

--- Project_1/src/project_1/Main.java
+++ Project_1/src/project_1/Main.java
@@ -14,7 +14,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Dashboard screen = new Dashboard();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
     }
     
 }
