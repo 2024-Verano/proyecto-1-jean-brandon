73804b216075805dba1533624cdc535a8623e731
brand <brand@Compu_Bran>
2024-12-31 02:02:16 -0600

    w

---
+++ Project_1/src/modelo/Workshop.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author brand
+ */
+public class Workshop {
+    private Integer service_code;
+    private Integer customer_code;
+    private String bike_brand;
+    private String bike_description;
+    private Integer price;
+    private Integer date_rece;
+    private Integer date_deli;
+    private String observations;
+    private boolean condition;        
+    
+    
+    
+    
+    
+    
+    public void createservice(){
+    
+    }
+    public void findcustomer(){
+    
+    }
+    public void modifyservice(){
+    
+    }
+    
+    public void deleteservice(){
+    
+    }
+    
+}
